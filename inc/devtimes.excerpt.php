533a186e8d8f03e62a5b44b257925f74efe7b477
eppz <hello@eppz.eu>
2017-06-26 15:50:05 +0200

    0.0.0

---
+++ inc/devtimes.excerpt.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function devtimes_excerpt_filter($excerpt)
+{
+    return Timber::fetch(
+        'devtimes.excerpt.twig',
+        array
+        (
+            'title' => get_the_title(),
+            'thumbnailHTML' => get_the_post_thumbnail(),
+            'excerpt' => $excerpt,
+            'permalink' => get_permalink(),
+        )
+    );
+}
+
+add_filter('get_the_excerpt', 'devtimes_excerpt_filter');
